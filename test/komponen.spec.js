0239f807d05abf5f05cdea0fa7df1ed7d86fb522
Ari Putra Utama <arieputrautama1998@gmail.com>
2023-05-20 09:51:07 +0700

    cc6

--- test/komponen.spec.js
+++ test/komponen.spec.js
@@ -2,45 +2,49 @@ const supertest = require("supertest");
 const app = require("../app.js");
 const truncate = require("../utils/truncate");
 
-truncate.component();
+truncate.komponen();
 
 const komponen = {
   komponen_id: "1",
   nama_komponen: "buku",
 };
-describe("TEST /komponen endpoint", () => {
-  // positive
-  test("Tambah komponen", async () => {
-    try {
-      const res = await supertest(app).get("/komponen");
 
-      expect(res.statusCode).toBe(200);
-      expect(res.body).toHaveProperty("status");
-      expect(res.body).toHaveProperty("message");
-      expect(res.body).toHaveProperty("data");
-      expect(res.body.data).toHaveProperty("id");
-      expect(res.body.data).toHaveProperty("name");
-      expect(res.body.data).toHaveProperty("email");
-      expect(res.body.status).toBe(true);
-      expect(res.body.message).toBe("user created komponen");
-    } catch (error) {
-      expect(error).toBe("error");
-    }
+// Komponen
+describe("TEST /auth/komponen endpoint", () => {
+  // positive
+  test("Komponen berhasil : menampilkan komponen", () => {
+    return supertest(app)
+      .post("/auth/komponen")
+      .send(komponen)
+      .then((res) => {
+        console.log(res.body);
+
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toHaveProperty("status");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body).toHaveProperty("data");
+        expect(res.body.data).toHaveProperty("id");
+        expect(res.body.data).toHaveProperty("name");
+        expect(res.body.data).toHaveProperty("email");
+        expect(res.body.status).toBe(true);
+        expect(res.body.message).toBe("user created komponen");
+      });
   });
 
   // negative
-  test("Tambah Komponen negativ", async () => {
-    try {
-      const res = await supertest(app).post("/komponen").send(komponen);
+  test("Komponen gagal : menampilkan komponen", () => {
+    return supertest(app)
+      .post("/auth/komponen")
+      .send(komponen)
+      .then((res) => {
+        console.log(res.body);
 
-      expect(res.statusCode).toBe(400);
-      expect(res.body).toHaveProperty("status");
-      expect(res.body).toHaveProperty("message");
-      expect(res.body).toHaveProperty("data");
-      expect(res.body.status).toBe(false);
-      expect(res.body.message).toBe("email already used!");
-    } catch (error) {
-      expect(error).toBe("error");
-    }
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty("status");
+        expect(res.body).toHaveProperty("message");
+        expect(res.body).toHaveProperty("data");
+        expect(res.body.status).toBe(false);
+        expect(res.body.message).toBe("komponen already used!");
+      });
   });
 });
